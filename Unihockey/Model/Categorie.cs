5cc738b4b53efe271f19122e7a044f8785a7233d
Alessandro BORRANI <BorraniA2@s2.rpn.ch>
2024-04-17 11:16:43 +0200

    Création du diagramme de classes et des classes dans le projet .NET MAUI

---
+++ Unihockey/Model/Categorie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unihockey.Model
+{
+    internal class Categorie
+    {
+        private int Id;
+        private string Nom;
+        private int DureePeriode;
+        private int NombrePeriode;
+
+
+    }
+}
---
+++ Unihockey/Model/Match.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unihockey.Model
+{
+    internal class Match
+    {
+        private int Id;
+        private Equipe Equipe1;
+        private Equipe Equipe2;
+        private DateTime Date;
+        private int ScoreEquipe1;
+        private int ScoreEquipe2;
+    }
+}
