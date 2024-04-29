8d67f16c5774f3991df1551044d721dc710def60
Alessandro BORRANI <BorraniA2@s2.rpn.ch>
2024-04-29 09:54:00 +0200

    Ajout d'un fichier avec tout le code sql

---
+++ script_psql/00-completMountCode.sql
@@ -0,0 +1,16 @@
+/*
+    @Author : Alessandro Borrani
+    Code sql pour la suppression de toutes les tables de la BD
+*/
+-- Suppression de tables
+
+DROP TABLE IF EXISTS Categorie CASCADE                          -- Suppression de la table Categorie en cascade
+;
+
+DROP TABLE IF EXISTS Equipe CASCADE                             -- Suppression de la table Equipe en cascade
+;
+
+DROP TABLE IF EXISTS Match CASCADE                              -- Suppression de la table Match en cascade
+;
+
+
