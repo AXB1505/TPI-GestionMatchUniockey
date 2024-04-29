6484e2e66c379a4c28d4847b96899da14bcea2b2
Alessandro BORRANI <BorraniA2@s2.rpn.ch>
2024-04-29 09:48:09 +0200

    Attribution du type SERIAL PRIMARY KEY au clées primaires et ajout de commentaire au script

--- script_psql/001-dropTable.sql
+++ script_psql/001-dropTable.sql
@@ -4,13 +4,13 @@
 */
 -- Suppression de tables
 
-DROP TABLE IF EXISTS Categorie CASCADE
+DROP TABLE IF EXISTS Categorie CASCADE                          -- Suppression de la table Categorie en cascade
 ;
 
-DROP TABLE IF EXISTS Equipe CASCADE
+DROP TABLE IF EXISTS Equipe CASCADE                             -- Suppression de la table Equipe en cascade
 ;
 
-DROP TABLE IF EXISTS Match CASCADE
+DROP TABLE IF EXISTS Match CASCADE                              -- Suppression de la table Match en cascade
 ;
 
 
--- script_psql/003-insertData.sql
+++ script_psql/003-insertData.sql
@@ -5,30 +5,30 @@
 -- Insertion de données
 -- Lieu
 -- Categorie
-INSERT INTO Categorie(num, nom, dureeMatch, nombrePeriode) VALUES
-    (1, 'Juniors E', '24000', 1),
-    (2, 'Juniors D', '20000', 2),
-    (3, 'Juniors C', '24000', 2),
-    (4, 'Juniors B', '20000', 2),
-    (5, 'Juniors A', '20000', 2),
-    (6, 'Femme', '20000', 2),
-    (7, 'Homme', '20000', 2)
+INSERT INTO Categorie(nom) VALUES
+    ('Juniors E' ),
+    ('Juniors D'),
+    ('Juniors C'),
+    ('Juniors B'),
+    ('Juniors A'),
+    ('Femme'),
+    ('Homme')
 ;
 -- Equipe
-INSERT INTO Equipe(num, nom, Cat_num) VALUES
-    (1, 'Fleurier Unihockey Club', 1),
-    (2, 'Klub FSC Corcelles-Cormondrèche', 1),
-    (3, 'UHC Le Rouge et Or', 1),
-    (4, 'UHC Bevaix', 1),
-    (5, 'UHC Cornaux', 1),
-    (6, 'UHC La Brévine', 1),
-    (7, 'UHC La Chaux-de-Fonds', 1)
+INSERT INTO Equipe(nom, Cat_num) VALUES
+    ('Fleurier Unihockey Club', 1),
+    ('Klub FSC Corcelles-Cormondrèche', 1),
+    ('UHC Le Rouge et Or', 1),
+    ('UHC Bevaix', 1),
+    ('UHC Cornaux', 1),
+    ('UHC La Brévine', 1),
+    ('UHC La Chaux-de-Fonds', 1)
 ;
 -- Resultat
-INSERT INTO Match(num, Equ_num1, Equ_num2, debutMatch, scoreEquipe1, scoreEquipe2) VALUES
-    (1, 1, 2, '2024-01-01 14:00:00', 2, 1),
-    (2, 3, 4, '2024-01-02 14:00:00', 3, 0),
-    (3, 5, 6, '2024-01-03 14:00:00', 1, 1),
-    (4, 7, 3, '2024-01-04 14:00:00', 0, 2),
-    (5, 2, 7, '2024-01-05 14:00:00', 1, 1)
+INSERT INTO Match(Equ_num1, Equ_num2, debutMatch, scoreEquipe1, scoreEquipe2) VALUES
+    (1, 2, '2024-01-01 14:00:00', 2, 1),
+    (3, 4, '2024-01-02 14:00:00', 3, 0),
+    (5, 6, '2024-01-03 14:00:00', 1, 1),
+    (7, 3, '2024-01-04 14:00:00', 0, 2),
+    (2, 7, '2024-01-05 14:00:00', 1, 1)
 ;
